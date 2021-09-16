ddc51250883aa063bb52cb33fce4c6a582a84cc5
christopher <coronac511@gmail.com>
2021-09-16 12:35:02 -0400

    pushing changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,27 @@
 class PostsController < ApplicationController
     def index  
         posts = Post.all
-        render json: posts
+        render json: posts, include: :user
       end 
     
       def show 
-        post = Post.find_by(id: params[:id])
+        post = Post.find_by(id: post_params[:id])
         render json: post, include: :comments
       end
     
       def create 
-        post = Post.create(post_params)
+        post = Post.create!(post_params)
         render json: post, status: :created
       end
     
       def update
-        post = Post.find_by(id: params[:id])
+        post = Post.find_by(id: post_params[:id])
         post.update(post_params)
         render json: post
       end 
     
       def destroy
-        post = Post.find_by(id: params[:id])
+        post = Post.find_by(id: post_params[:id])
         post.destroy
         head :no_content
       end
